0984062d5e961edcbf28a8eeeb7e26d46d32a81f
Riccardo Gerosa <r.gerosa@gmail.com>
2016-09-04 21:50:25 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-qemu-system-arm -curses \
-	-kernel kernel-qemu \
-	-cpu arm1176 \
-	-m 256 \
-	-M versatilepb \
-	-serial stdio \
-	-append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
-	-drive "file=disk.img,index=0,media=disk,format=raw"
+qemu-system-arm \
+        -kernel kernel-qemu \
+        -cpu arm1176 \
+        -m 256 \
+        -M versatilepb \
+        -serial stdio \
+        -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
+        -hda disk.img \
+        -nographic \
+        -redir tcp:2222::22
