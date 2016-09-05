c2d72b612cac75a79d8e7f7dac7f11d2e4db08fb
Riccardo Gerosa <r.gerosa@gmail.com>
2016-09-05 09:01:24 -0500

    Update provision3.sh

--- provision3.sh
+++ provision3.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-#mkdir tmp1
-#mount -o loop,offset=4194304 disk.img tmp1
-#cp tmp1/kernel7.img ./
-#cp tmp1/*.dtb ./
-#umount tmp1
-#rm -r tmp1
+imgStart=$(fdisk -lu disk.img | grep "Linux" | awk '{print $2}')
+secSize=$(fdisk -lu disk.img | grep "Units:" | awk '{print $8}')
+offset=$((imgStart * secSize))
+echo "Disk image offset is" $imgStart "*" $secSize "=" $offset
 
+echo "Modifying disk image to run in Qemu"
 mkdir tmp2
-mount -t auto -o loop,offset=70254592 disk.img tmp2
+mount -t auto -o loop,offset=$offset disk.img tmp2
 echo "#/usr/lib/arm-linux-gnueabihf/libarmmem.so" >tmp2/etc/ld.so.preload
 echo 'KERNEL=="sda", SYMLINK+="mmcblk0"' >tmp2/etc/udev/rules.d/90-qemu.rules
 echo 'KERNEL=="sda?", SYMLINK+="mmcblk0p%n"' >>tmp2/etc/udev/rules.d/90-qemu.rules
