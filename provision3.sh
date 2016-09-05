775d66568f268f87cf92b216af990a6cdbe2737f
Riccardo Gerosa <r.gerosa@gmail.com>
2016-09-04 22:10:48 -0500

    Update provision3.sh

--- provision3.sh
+++ provision3.sh
@@ -13,6 +13,7 @@ echo "#/usr/lib/arm-linux-gnueabihf/libarmmem.so" >tmp2/etc/ld.so.preload
 echo 'KERNEL=="sda", SYMLINK+="mmcblk0"' >tmp2/etc/udev/rules.d/90-qemu.rules
 echo 'KERNEL=="sda?", SYMLINK+="mmcblk0p%n"' >>tmp2/etc/udev/rules.d/90-qemu.rules
 echo 'KERNEL=="sda2", SYMLINK+="root"' >>tmp2/etc/udev/rules.d/90-qemu.rules
+sed -e '/mmcblk/ s/^#*/#/' -i tmp2/etc/fstab
 sync
 umount tmp2
 rm -r tmp2
